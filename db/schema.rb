893368d619de2867c25f429f5a6280001cf93d3b
Louis Portier <lpo31100@gmail.com>
2018-03-14 20:12:26 +1100

    added price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314083249) do
+ActiveRecord::Schema.define(version: 20180314090800) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180314083249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
+    t.integer "price"
   end
 
 end
