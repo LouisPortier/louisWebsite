28c067de43eccb4d6b8e94097c4f8972a65ddbab
Louis Portier <lpo31100@gmail.com>
2018-03-18 18:40:41 +1100

    Changed the CSS for product/show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,19 +1,20 @@
 class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
+	
 	def self.search(search_term)
-  	if Rails.env.development?
-      @products = Product.where("name LIKE ?", "%#{search_term}%")
-    else
-      @products = Product.where("name ilike ?", "%#{search_term}%")
-    end
+		if Rails.env.development?
+			@products = Product.where("name LIKE ?", "%#{search_term}%")
+		else
+			@products = Product.where("name ilike ?", "%#{search_term}%")
+		end
 	end
 
 	def highest_rating_comment
-  	comments.rating_desc.first
+		comments.rating_desc.first
 	end
 
 	def lowest_rating_comment
-  	comments.rating_asc.first
+		comments.rating_asc.first
 	end
 end
