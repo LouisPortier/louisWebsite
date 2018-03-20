31c307437c8775611834240ab31591e45b4e5c92
Louis Portier <lpo31100@gmail.com>
2018-03-20 18:50:24 +1100

    fixed authorizations

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new
-    can :manage, User, id: user.id
-    cannot [:destroy], Comment
+    user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
+    else
+      can :read, :all
+      cannot :manage, User
+      can :manage, User, id: user.id
+      cannot :index, User
+      can [:read, :create], Comment
+      cannot [:destroy], Comment
+      can [:read], Product
+      cannot [:edit, :create, :update, :destroy], Product
     end
   end
 end
